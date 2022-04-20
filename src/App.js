d62dcd96ca7551f892ff7244f9243afe08c9664e
monirulakand <soft.monirul@gmail.com>
2022-04-20 19:00:33 +0600

    boiler plate ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-44 h-44 justify-center rounded-2xl text-center bg-black">
+
+      <h3 className="text-white text-2xl py-6">hello</h3>
+        
+    </div>
+  );
+}
+
+export default App;
