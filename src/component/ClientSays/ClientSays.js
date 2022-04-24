b0846bbd2a97d001037a51fb4de548e45d565797
monirulakand <soft.monirul@gmail.com>
2022-04-24 16:32:27 +0600

    navbar design done, home banner design done, client section design done

--- src/component/ClientSays/ClientSays.js
+++ src/component/ClientSays/ClientSays.js
@@ -1,42 +1,67 @@
 import React, { Component, Fragment } from 'react';
 import Client1 from "../../asset/images/client/client1.jpg";
-
+import { FaStar } from "react-icons/fa";
 class ClientSays extends Component {
     render() {
         return (
             <Fragment>
-                <div className="container mt-8 mb-8">
+                <div className="container lg:mt-8 lg:mb-12">
                     <h4 className="text-4xl	font-semibold ml-3 text-sectionTitleColor">What Our Client Say</h4>
-                    <div class="flex flex-auto mt-3 p-3">
+                    <div class="flex flex-auto lg:mt-6 p-3">
 
-                        <div class="lg:basis-1/3 mx-2">
-                            <div class="bg-white rounded-2xl shadow-md border-none p-6 w-auto">
-                                <img className="rounded-full h-24 w-24" src={Client1}/>
-                                <h5 class="text-md font-normal text-clientsectiondes leading-8 mb-4 mt-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h5>
-                                <p class="text-gray-700 text-sm">Jessica Strike</p>
-                                <p class="text-gray-700 text-sm">Student</p>
+                        <div class="lg:basis-1/3 mx-4">
+                            <div class="bg-white rounded-2xl shadow-client border-none lg:p-12 w-auto">
+                                <img className="rounded-full h-36 w-36 mt-8 mb-12" src={Client1} />
+                                <h5 class="text-base font-normal text-client-section-des leading-8 mb-4 mt-0">Lorem Ipsum is simply dummy printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</h5>
+                                <ul className="flex sm:justify-center xl:justify-start mt-2">
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-1 text-sm font-normal text-client-section-des">( 4.5 )</h6>
+                                </ul>
+                                <p class="text-sectionTitleColor text-2xl font-semibold">Jessica Strike</p>
+                                <p class="text-client-section-des text-md mt-2">Student</p>
                             </div>
                         </div>
 
-                        <div class="lg:basis-1/3 mx-2">
-                            <div class="bg-white rounded-2xl shadow-md border-none p-6 w-auto">
-                                <img className="rounded-full h-24 w-24" src={Client1}/>
-                                <h5 class="text-md font-normal text-clientsectiondes leading-8 mb-4 mt-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h5>
-                                <p class="text-gray-700 text-sm">Jessica Strike</p>
-                                <p class="text-gray-700 text-sm">Student</p>
+                        <div class="lg:basis-1/3 mx-4">
+                            <div class="bg-white rounded-2xl shadow-client border-none lg:p-12 w-auto">
+                                <img className="rounded-full h-36 w-36 mt-8 mb-12" src={Client1} />
+                                <h5 class="text-base font-normal text-client-section-des leading-8 mb-4 mt-0">Lorem Ipsum is simply dummy printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</h5>
+                                <ul className="flex sm:justify-center xl:justify-start mt-2">
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-1 text-sm font-normal text-client-section-des">( 4.5 )</h6>
+                                </ul>
+                                <p class="text-sectionTitleColor text-2xl font-semibold">Jessica Strike</p>
+                                <p class="text-client-section-des text-md mt-2">Student</p>
                             </div>
                         </div>
 
-                        <div class="lg:basis-1/3 mx-2">
-                            <div class="bg-white rounded-2xl shadow-md border-none p-6 w-auto">
-                                <img className="rounded-full h-24 w-24" src={Client1}/>
-                                <h5 class="text-md font-normal text-clientsectiondes leading-8 mb-4 mt-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h5>
-                                <p class="text-gray-700 text-sm">Jessica Strike</p>
-                                <p class="text-gray-700 text-sm">Student</p>
+
+                        <div class="lg:basis-1/3 mx-4">
+                            <div class="bg-white rounded-2xl shadow-client border-none lg:p-12 w-auto">
+                                <img className="rounded-full h-36 w-36 mt-8 mb-12" src={Client1} />
+                                <h5 class="text-base font-normal text-client-section-des leading-8 mb-4 mt-0">Lorem Ipsum is simply dummy printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</h5>
+                                <ul className="flex sm:justify-center xl:justify-start mt-2">
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                    <h6 className="mb-4 mx-1 text-sm font-normal text-client-section-des">( 4.5 )</h6>
+                                </ul>
+                                <p class="text-sectionTitleColor text-2xl font-semibold">Jessica Strike</p>
+                                <p class="text-client-section-des text-md mt-2">Student</p>
                             </div>
                         </div>
-                    
-                        
+
+
                     </div>
                 </div>
             </Fragment>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,11 +3,15 @@ import CopyRight from '../../component/Common/CopyRight/CopyRight';
 import Footer from '../../component/Common/Footer/Footer';
 import Partner from '../../component/Home/Partner/Partner';
 import ClientSays from '../../component/ClientSays/ClientSays';
+import NavBarTop from '../../component/Common/NavBarTop/NavBarTop';
+import HomeBanner from '../../component/Home/HomeBanner/HomeBanner';
 
 class HomePage extends Component {
     render() {
         return (
             <Fragment>
+                <NavBarTop/>
+                <HomeBanner/>
                 <ClientSays/>
                 <Partner/>
                 <Footer />
