41c0b782813d3a318f342e9914f9ba999e3c8036
monirulakand <soft.monirul@gmail.com>
2022-05-14 15:27:48 +0600

    home page recent course tab add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+
+    "./node_modules/flowbite/**/*.js",
+
   ],
   theme: {
     extend: {},
@@ -9,31 +12,34 @@ module.exports = {
       'black': '#000000',
       'maincolor': '#D76E2D',
       'maincolordeep': "#B5581F",
-      'LiveBtnColor':'#FC4646',
-      'SpeedMeter':'#7CCC1F',
+      'LiveBtnColor': '#FC4646',
+      'SpeedMeter': '#7CCC1F',
       'ShareColor': '##7CCC1F',
-      'BgLoveIcon':'#F9F9FC',
+      'BgLoveIcon': '#F9F9FC',
       'CertificateColor': '#F5A625',
-      'CourseTitle':'#1D2733',
+      'CourseTitle': '#1D2733',
       'btngreen': '#39741F',
       'sectionTitleColor': "#1D2733",
       'maingray': "#1D2733",
       'client-section-des': '#A5A7AB',
-      'des-gray':'#414853',
-      'facebook-color':'#3B5998',
-      'twitter-color':'#00ACEE',
-      'google-color':'#EF213D',
-      'login-bg-color':'#F5EAC8',
+      'des-gray': '#414853',
+      'facebook-color': '#3B5998',
+      'twitter-color': '#00ACEE',
+      'google-color': '#EF213D',
+      'login-bg-color': '#F5EAC8',
       'amber': {
         300: '#fcd34d',
         400: '#fbbf24',
       },
-      
+
     },
     container: {
       center: true,
     },
   },
-  plugins: [],
+  
+  plugins: [
+    require('flowbite/plugin')
+  ]
 
 }
