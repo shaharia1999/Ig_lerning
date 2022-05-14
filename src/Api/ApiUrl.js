ec25882fc158a0250647d94db812101417ea3490
mdsozon45-source <mdsozon45@gmail.com>
2022-05-14 19:59:37 +0600

    git some api done

--- src/Api/ApiUrl.js
+++ src/Api/ApiUrl.js
@@ -1,5 +1,5 @@
 class ApiUrl{
-    // static BaseUrl="http://127.0.0.1:8000/";
-    static BaseUrl="http://192.168.1.4:8002/";
+    static BaseUrl="http://127.0.0.1:8000/";
+    // static BaseUrl="http://192.168.1.4:8002/";
 }
 export default ApiUrl;
