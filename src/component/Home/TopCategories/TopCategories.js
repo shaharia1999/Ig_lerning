6247da7f1e2694f30485d891481acc8cf345591d
joy1954islam <joyislam1954@gmail.com>
2022-05-15 14:52:21 +0600

    top categories loading done

--- src/component/Home/TopCategories/TopCategories.js
+++ src/component/Home/TopCategories/TopCategories.js
@@ -4,15 +4,15 @@ import ApiUrl from "../../../Api/ApiUrl";
 import ShareIcon from "../../../asset/images/icon/icon_share.png";
 
 
-
 function TopCategories() {
+    
     // let language = document.getElementById('languagess');
     // console.log('language = ', language);
-    const custom_headers =  { 
-        method: 'get', 
-        
+    const custom_headers = {
+        method: 'get',
+
         headers: new Headers({
-            'Accept-Language': 'en', 
+            'Accept-Language': 'en',
             'Content-Type': 'application/json'
         })
     }
@@ -21,29 +21,46 @@ function TopCategories() {
         await (await fetch(ApiUrl.BaseUrl + "api/v2/top-category-info/", custom_headers)).json();
     const { data, error, status, isLoading, isError, isLoadingError } = useQuery("users", fetchTopCategories);
     const extra = useQuery("users", fetchTopCategories);
-    console.log("dt =", data)
-    console.log("Check Values =", extra)
-    
 
-    const waitdata = []
-
-    if (status === 'loading'){
-        //if (typeof(data) === 'undefined'){
+    if (status === 'loading') {
         return (
             <div className="container my-12">
                 <h4 className="text-4xl	font-semibold text-sectionTitleColor ml-3">Top Categories </h4>
                 <div className="flex flex-wrap -mx-1 lg:-mx-4">
-                    <h1>Loading</h1>
+                    <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
+                        <article className="overflow-hidden rounded-lg shadow-lg hover:opacity-30 hover:bg-black">
+                            <div className="ph-picture block bg-fixed h-72 w-full"></div>
+                            <h1 className=" lg:text-xl lg:font-semibold text-center  text-maingray lg:mt-4 lg:mb-4">
+                                <div className="ph-col-6 pt-2 pb-2"></div>
+                            </h1>
+                        </article>
+                    </div>
+                    <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
+                        <article className="overflow-hidden rounded-lg shadow-lg hover:opacity-30 hover:bg-black">
+                            <div className="ph-picture block bg-fixed h-72 w-full"></div>
+                            <h1 className=" lg:text-xl lg:font-semibold text-center  text-maingray lg:mt-4 lg:mb-4">
+                                <div className="ph-col-6 pt-2 pb-2"></div>
+                            </h1>
+                        </article>
+                    </div>
+                    <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
+                        <article className="overflow-hidden rounded-lg shadow-lg hover:opacity-30 hover:bg-black">
+                            <div className="ph-picture block bg-fixed h-72 w-full"></div>
+                            <h1 className=" lg:text-xl lg:font-semibold text-center  text-maingray lg:mt-4 lg:mb-4">
+                                <div className="ph-col-6 pt-2 pb-2"></div>
+                            </h1>
+                        </article>
+                    </div>
                 </div>
             </div>
         );
-    }else if(status === 'success'){
+    } else if (status === 'success') {
         console.log("Data Found ", data)
         const html_dt = data.data.map((val, index) => (
-            <div  className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
+            <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
                 <article className="overflow-hidden rounded-lg shadow-lg hover:opacity-30 hover:bg-black">
                     <img alt="Placeholder" className="block bg-fixed h-72 w-full" src={val.category_preview_img} />
-                    <h1 className="lg:text-xl lg:font-semibold text-center  text-maingray lg:mt-4 lg:mb-4">{val.category_name}</h1>
+                    <h1 className="lg:text-xl lg:font-semibold text-center text-maingray lg:mt-4 lg:mb-4">{val.category_name}</h1>
                 </article>
             </div>
         ))
@@ -51,16 +68,13 @@ function TopCategories() {
             <div className="container my-12">
                 <h4 className="text-4xl	font-semibold text-sectionTitleColor ml-3">Top Categories {data.category_count} </h4>
                 <div className="flex flex-wrap -mx-1 lg:-mx-4">
-                {
-                    html_dt
-                }
+                    {
+                        html_dt
+                    }
                 </div>
             </div>
         );
     }
-
-    
-    
 }
 
 export default TopCategories;
