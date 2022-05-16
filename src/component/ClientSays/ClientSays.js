ab8f2456d518599f8201a72896bc55afd624733c
mdsozon45-source <mdsozon45@gmail.com>
2022-05-16 16:22:40 +0600

    [200~ClientSays

--- src/component/ClientSays/ClientSays.js
+++ src/component/ClientSays/ClientSays.js
@@ -1,72 +1,99 @@
 import React, { Component, Fragment } from 'react';
 import Client1 from "../../asset/images/client/client1.jpg";
 import { FaStar } from "react-icons/fa";
-class ClientSays extends Component {
-    render() {
-        return (
-            <Fragment>
-                <div className="container lg:mt-8 lg:mb-12">
-                    <h4 className="text-4xl	font-semibold ml-3 text-sectionTitleColor">What Our Client Say</h4>
-                    <div class="flex flex-auto lg:mt-6 p-3">
-
-                        <div class="lg:basis-1/3 mx-4">
-                            <div class="bg-white rounded-2xl shadow-client border-none lg:p-12 w-auto">
-                                <img className="rounded-full h-36 w-36 mt-8 mb-12" src={Client1} />
-                                <h5 class="text-base font-normal text-client-section-des leading-8 mb-4 mt-0">Lorem Ipsum is simply dummy printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</h5>
-                                <ul className="flex sm:justify-center xl:justify-start mt-2">
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-1 text-sm font-normal text-client-section-des">( 4.5 )</h6>
-                                </ul>
-                                <p class="text-sectionTitleColor text-2xl font-semibold">Jessica Strike</p>
-                                <p class="text-client-section-des text-md mt-2">Student</p>
-                            </div>
-                        </div>
-
-                        <div class="lg:basis-1/3 mx-4">
-                            <div class="bg-white rounded-2xl shadow-client border-none lg:p-12 w-auto">
-                                <img className="rounded-full h-36 w-36 mt-8 mb-12" src={Client1} />
-                                <h5 class="text-base font-normal text-client-section-des leading-8 mb-4 mt-0">Lorem Ipsum is simply dummy printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</h5>
-                                <ul className="flex sm:justify-center xl:justify-start mt-2">
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-1 text-sm font-normal text-client-section-des">( 4.5 )</h6>
-                                </ul>
-                                <p class="text-sectionTitleColor text-2xl font-semibold">Jessica Strike</p>
-                                <p class="text-client-section-des text-md mt-2">Student</p>
-                            </div>
-                        </div>
+import ApiUrl from '../../Api/ApiUrl';
+import { useQuery } from "react-query";
 
 
-                        <div class="lg:basis-1/3 mx-4">
-                            <div class="bg-white rounded-2xl shadow-client border-none lg:p-12 w-auto">
-                                <img className="rounded-full h-36 w-36 mt-8 mb-12" src={Client1} />
-                                <h5 class="text-base font-normal text-client-section-des leading-8 mb-4 mt-0">Lorem Ipsum is simply dummy printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</h5>
-                                <ul className="flex sm:justify-center xl:justify-start mt-2">
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
-                                    <h6 className="mb-4 mx-1 text-sm font-normal text-client-section-des">( 4.5 )</h6>
-                                </ul>
-                                <p class="text-sectionTitleColor text-2xl font-semibold">Jessica Strike</p>
-                                <p class="text-client-section-des text-md mt-2">Student</p>
-                            </div>
-                        </div>
-
-
-                    </div>
-                </div>
-            </Fragment>
-        );
+function ClientSays(){
+    const cliensays_custom_headers =  { 
+        method: 'get',
+        headers: new Headers({
+            'Accept-Language': 'bn', 
+            'Content-Type': 'application/json'
+        })
     }
+
+    const fetchClientSays = async () =>
+        await (await fetch(ApiUrl.BaseUrl + "api/course/what-our-client-say/", cliensays_custom_headers)).json();
+    const says_client = useQuery("says", fetchClientSays);
+    const says_client_data = says_client;
+
+    if (says_client.status === 'loading'){
+        return (
+            <div className="container my-12">
+                <h4 className="text-4xl	font-semibold text-sectionTitleColor ml-3">loading</h4>
+                <div className="flex flex-wrap -mx-1 lg:-mx-4">
+                    <h1>Loading</h1>
+                </div>
+            </div>
+        );
+    }else if (says_client.status === 'success'){
+        const html_says_client = says_client_data.data.data.map((says_client, index) => (
+            <div class="lg:basis-1/3 px-4">
+                <div class="bg-white rounded-2xl shadow-client border-none lg:p-12 h-full w-auto">
+                    <img className="rounded-full h-36 w-36 mt-8 mb-12" src={ApiUrl.ImageBaseUrl+ says_client.user_info.image} />
+                    <h5 class="text-base font-normal text-client-section-des leading-8 mb-4 mt-0">{says_client.review_description}</h5>
+                    
+                    {(() => {
+                        if(says_client.rating === 5){
+                            return <ul className="flex sm:justify-center xl:justify-start mt-2">
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-1 text-sm font-normal text-client-section-des">( {says_client.rating} )</h6>
+                            </ul> 
+                        }else if(says_client.rating === 4){
+                            return <ul className="flex sm:justify-center xl:justify-start mt-2">
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-1 text-sm font-normal text-client-section-des">( {says_client.rating} )</h6>
+                            </ul> 
+                        }else if(says_client.rating === 3){
+                            return <ul className="flex sm:justify-center xl:justify-start mt-2">
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-1 text-sm font-normal text-client-section-des">( {says_client.rating} )</h6>
+                            </ul> 
+                        }else if(says_client.rating === 2){
+                            return <ul className="flex sm:justify-center xl:justify-start mt-2">
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-1 text-sm font-normal text-client-section-des">( {says_client.rating} )</h6>
+                            </ul> 
+                        }else if(says_client.rating === 1){
+                            return <ul className="flex sm:justify-center xl:justify-start mt-2">
+                                <h6 className="mb-4 mx-.75"><FaStar className="text-amber-400" /></h6>
+                                <h6 className="mb-4 mx-1 text-sm font-normal text-client-section-des">( {says_client.rating} )</h6>
+                            </ul> 
+                        }
+                            
+                    })()}
+                    
+                    <p class="text-sectionTitleColor text-2xl font-semibold">{says_client.user_info.username}</p>
+                    <p class="text-client-section-des text-md mt-2">Student</p>
+                </div>
+            </div>
+        ))
+
+    return (
+        <Fragment>
+            <div className="container lg:mt-8 lg:mb-12">
+                <h4 className="text-4xl	font-semibold ml-3 text-sectionTitleColor">What Our Client Say</h4>
+                <div class="flex flex-wrap lg:mt-6 p-3">
+                    {
+                        html_says_client
+                    }
+                </div>
+            </div>
+        </Fragment>
+    );
+}
 }
 
 export default ClientSays;
--- src/component/Home/MostVisited/MostVisited.js
+++ src/component/Home/MostVisited/MostVisited.js
@@ -22,8 +22,9 @@ function MostVisited() {
     const fetchMostVisited = async () =>
         await (await fetch(ApiUrl.BaseUrl + "api/course/most-visited-course/", mostvisited_custom_headers)).json();
     const mostvisited = useQuery("country", fetchMostVisited);
-    const mostvisited_response_data = mostvisited.data;
-
+    const mostvisited_response_data = mostvisited;
+    console.log('mostvisited_response_data= ', mostvisited_response_data.data);
+    // console.log('most visite course=',mostvisited_response_data.channel_name_id)
     if (mostvisited.status === 'loading'){
         return (
             <div className="container my-12">
@@ -34,7 +35,8 @@ function MostVisited() {
             </div>
         );
     }else if (mostvisited.status === 'success'){
-        const html_mostvisited = mostvisited_response_data.data.map((mostvisited, index) => (
+        const html_mostvisited = mostvisited_response_data.data.data.map((mostvisited, index) => (
+            
             // <option value={mostvisited.course_id}>{mostvisited.course_title}</option>
 
             <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
@@ -42,7 +44,7 @@ function MostVisited() {
 
                     <div className="relative">
                         
-                        <iframe className="w-full h-72 object-cover object-center rounded-lg shadow-md" src="https://www.youtube.com/embed/j5Zsa_eOXeY"></iframe>
+                        <iframe className="w-full h-72 object-cover object-center rounded-lg shadow-md" src={mostvisited.promotional_video}></iframe>
                         <div className="flex flex-wrap">
                             <div className="w-1/5">
                                 <a href="!#"><div class="text-sm absolute top-0 left-2 bg-black px-4 text-white rounded-full h-12 w-12 flex flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
@@ -51,7 +53,7 @@ function MostVisited() {
                             </div>
                             <div className="w-4/5">
                                 <a href="!#"><div class="text-lg absolute top-0 text-white mt-5">
-                                    <h6 className="font-medium text-lg text-white">{mostvisited.channel_name_idchannel_name}</h6>
+                                    <h6 className="font-medium text-lg text-white">{mostvisited.channel_name_id.channel_name}</h6>
                                     <h6 className="font-light text-base text-white">{mostvisited.course_duration} min</h6>
                                 </div></a>
                             </div>
@@ -63,7 +65,7 @@ function MostVisited() {
                         <div className="bg-white p-3 rounded-lg shadow-lg">
                             <div className="flex flex-wrap">
                                 <div className=" w-4/5">
-                                    <h4 className="mt-1 text-lg font-semibold text leading-tight text-CourseTitle">Traning English Course Learn Online</h4>
+                                    <h4 className="mt-1 text-lg font-semibold text leading-tight text-CourseTitle">{mostvisited.course_title}</h4>
                                 </div>
                                 <div className="w-1/5">
                                     <button className="text-sm font-extralight leading-tight bg-LiveBtnColor text-white lg:pl-3 lg:pr-3 lg:pt-1 lg:pb-1 rounded-md">Live</button>
@@ -119,7 +121,7 @@ function MostVisited() {
         return (
             <Fragment>
                 <div className="container my-12">
-                    <h4 className="text-4xl	font-semibold text-sectionTitleColor ml-3">Most Visited ({mostvisited_response_data.most_visited_count}) </h4>
+                    <h4 className="text-4xl	font-semibold text-sectionTitleColor ml-3">Most Visited ({mostvisited_response_data.data.most_visited_count}) </h4>
                     <div className="flex flex-wrap -mx-1 lg:-mx-4">
                         {
                             html_mostvisited
