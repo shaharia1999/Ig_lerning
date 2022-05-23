10f84cb3142c6c8a89e6af35eb4f7d70d3e02cd5
joy1954islam <joyislam1954@gmail.com>
2022-05-23 19:03:46 +0600

    loading page

--- src/component/Home/TopCategories/TopCategories.js
+++ src/component/Home/TopCategories/TopCategories.js
@@ -38,26 +38,28 @@ function TopCategories() {
     const Topcategory = (() => {
         console.log('is loading4 = ', isLoading);
         if (isLoading === true) {
-            <div>
-                <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
-                    <article className="overflow-hidden rounded-lg shadow-lg hover:opacity-30 hover:bg-black">
-                        <img alt="Placeholder" className="block bg-fixed h-72 w-full" src={CourseLogo}/>
-                        <h1 className="lg:text-xl lg:font-semibold text-center text-maingray lg:mt-4 lg:mb-4">a</h1>
-                    </article>
+            return (
+                <div className="flex flex-wrap -mx-1 lg:-mx-4">
+                    <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
+                        <article className="overflow-hidden rounded-lg shadow-lg hover:opacity-30 hover:bg-black">
+                            <div alt="Placeholder" className="animate-pulse block bg-fixed h-72 w-full" />
+                            <h1 className="h-2 bg-slate-200 rounded lg:text-xl lg:font-semibold text-center text-maingray lg:mt-4 lg:mb-4"></h1>
+                        </article>
+                    </div>
+                    <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
+                        <article className="overflow-hidden rounded-lg shadow-lg hover:opacity-30 hover:bg-black">
+                            <img alt="Placeholder" className="block bg-fixed h-72 w-full" src={CourseLogo} />
+                            <h1 className="lg:text-xl lg:font-semibold text-center text-maingray lg:mt-4 lg:mb-4">a</h1>
+                        </article>
+                    </div>
+                    <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
+                        <article className="overflow-hidden rounded-lg shadow-lg hover:opacity-30 hover:bg-black">
+                            <img alt="Placeholder" className="block bg-fixed h-72 w-full" src={CourseLogo} />
+                            <h1 className="lg:text-xl lg:font-semibold text-center text-maingray lg:mt-4 lg:mb-4">a</h1>
+                        </article>
+                    </div>
                 </div>
-                <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
-                    <article className="overflow-hidden rounded-lg shadow-lg hover:opacity-30 hover:bg-black">
-                        <img alt="Placeholder" className="block bg-fixed h-72 w-full" src={CourseLogo} />
-                        <h1 className="lg:text-xl lg:font-semibold text-center text-maingray lg:mt-4 lg:mb-4">a</h1>
-                    </article>
-                </div>
-                <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
-                    <article className="overflow-hidden rounded-lg shadow-lg hover:opacity-30 hover:bg-black">
-                        <img alt="Placeholder" className="block bg-fixed h-72 w-full" src={CourseLogo} />
-                        <h1 className="lg:text-xl lg:font-semibold text-center text-maingray lg:mt-4 lg:mb-4">a</h1>
-                    </article>
-                </div>
-            </div>
+            )
         }
         else if (isLoading === false) {
             return (
