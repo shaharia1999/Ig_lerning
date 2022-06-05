a7d51c55b949d5304e04a30af8e94307e2ab9d8e
monirulakand <soft.monirul@gmail.com>
2022-06-05 18:53:56 +0600

    top category, client says, most visited, recently added course, recommand course, most popular course, in base of interest, only on intellogeek, some bug fix

--- src/component/ClientSays/ClientSays.js
+++ src/component/ClientSays/ClientSays.js
@@ -1,13 +1,18 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, Fragment, useRef } from 'react';
 import Client1 from "../../asset/images/client/client1.jpg";
 import { FaStar } from "react-icons/fa";
 import ApiUrl from '../../Api/ApiUrl';
 import axios from "axios";
-
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function ClientSays() {
     const [says_client, setClientSays] = React.useState([]);
     const [isLoading, setIsLoading] = useState(false);
+
+    const [nav1, setNav1] = useState();
+    const slider1 = useRef();
 
     useEffect(() => {
         setIsLoading(true);
@@ -63,10 +68,10 @@ function ClientSays() {
         else if (isLoading === false) {
             return (
                 says_client.map((says_client, index) => (
-                    <div key={index} class="lg:basis-1/3 px-4">
+                    <div key={index} class="xl:w-1/3 px-4">
                         <div class="bg-white rounded-2xl shadow-client border-none lg:p-12 h-full w-auto">
-                            <img className="rounded-full h-36 w-36 mt-8 mb-12" src={ApiUrl.ImageBaseUrl + says_client.user_info.image} alt=''/>
-                            <h5 class="text-base font-normal text-client-section-des leading-8 mb-4 mt-0">{says_client.review_description}</h5>
+                            <img className="rounded-full h-36 w-36 mt-8 mb-12" src={ApiUrl.ImageBaseUrl + says_client.user_info.image} alt='' />
+                            <h5 class="text-base font-normal text-client-section-des leading-8 mb-4 mt-0">{`${says_client.review_description.substring(0, 250)}...`}</h5>
                             {(() => {
                                 if (says_client.rating === 5) {
                                     return <ul className="flex sm:justify-center xl:justify-start mt-2">
@@ -118,13 +123,33 @@ function ClientSays() {
         <Fragment>
             <div className="container lg:mt-8 lg:mb-12">
                 <h4 className="text-4xl	font-semibold ml-3 text-sectionTitleColor">What Our Client Say</h4>
-                <div class="flex flex-wrap lg:mt-6 p-3">
-                    {
-                        html_says_client
-                    }
+
+                <div className="lg:mt-6">
+                    <Slider
+                        ref={(slider1) => setNav1(slider1)}
+                        slidesToShow={3}
+                        dots={true}
+                        adaptiveHeight={true}
+                        infinite={true}
+                        slidesToScroll={1}
+                        loop={true}
+                        speed={1000}
+                        height={600}
+                        autoplay={true}
+                        autoplaySpeed={1500}
+                        swipeToSlide={true}
+                    >
+
+                        {
+                            html_says_client
+                        }
+                    </Slider>
                 </div>
+
+
+
             </div>
-        </Fragment>
+        </Fragment >
     );
 
 }
--- src/component/Home/TopCategories/TopCategories.js
+++ src/component/Home/TopCategories/TopCategories.js
@@ -1,12 +1,17 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect, Fragment, useRef } from "react";
 import ApiUrl from "../../../Api/ApiUrl";
 import axios from "axios";
-import CourseLogo from "../../../asset/images/logo/course-logo.png";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function TopCategories() {
     const [topCategory, setTopCategory] = useState([]);
     const [totalTopCategory, setTotalTopCategory] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+
+    const [nav3, setNav3] = useState();
+    const slider3 = useRef();
 
     function getTopCategory() {
         setIsLoading(true);
@@ -24,7 +29,7 @@ function TopCategories() {
 
     const Topcategory = (() => {
         if (isLoading === true) {
-              return <div className="flex flex-basis w-full -mx-1 lg:-mx-4">
+            return <div className="flex flex-basis w-full -mx-1 lg:-mx-4">
                 <div className="my-1 px-1 w-full md:w-1/2 lg:my-8 lg:px-5 lg:w-1/3">
                     <article className="overflow-hidden rounded-lg shadow-lg hover:opacity-30 animate-pulse">
                         <div class="block bg-fixed h-72 w-full w-200 h-200 p-2 bg-gray-200 rounded-md animate-pulse-slow"></div>
@@ -64,10 +69,28 @@ function TopCategories() {
         <Fragment>
             <div className="container my-12">
                 <h4 className="text-4xl	font-semibold text-sectionTitleColor ml-3">Top Categories ({totalTopCategory}) </h4>
-                <div className="flex flex-wrap -mx-1 lg:-mx-4">
-                    {
-                        Topcategory
-                    }
+                <div className="xl:mt-4">
+
+                    <Slider
+                        ref={(slider3) => setNav3(slider3)}
+                        slidesToShow={3}
+                        dots={true}
+                        adaptiveHeight={true}
+                        infinite={true}
+                        slidesToScroll={1}
+                        loop={true}
+                        speed={1000}
+                        autoplay={true}
+                        autoplaySpeed={1500}
+                        swipeToSlide={true}
+                        rows={2}
+
+                    >
+                        {
+                            Topcategory
+                        }
+                    </Slider>
+
                 </div>
             </div>
 
