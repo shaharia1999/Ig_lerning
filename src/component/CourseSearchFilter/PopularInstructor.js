d36fff238cc06393edfccf37bdf223e343541b7e
joy1954islam <joyislam1954@gmail.com>
2022-10-25 12:35:10 +0600

    instructors card designed

--- src/component/CourseSearchFilter/PopularInstructor.js
+++ src/component/CourseSearchFilter/PopularInstructor.js
@@ -6,110 +6,114 @@ const PopularInstructor = () => {
         <div>
             <h4>Popular Instructors</h4>
             <div className='grid grid-cols-5 mx-10 gap-5'>
-                <div className='text-center bg-gray-400 rounded-lg py-10'>
+                <div className='text-center bg-white rounded-lg py-6 px-3 border-2'>
                     <div className='relative flex flex-col items-center'>
                         <div class="avatar relative">
-                            <div class="w-24 -z-1 rounded-full ring ring-warning ring-offset-base-100 ring-offset-2">
+                            <div class="w-24 -z-1 rounded-full ring ring-[#D76E2D] ring-offset-base-100 ring-offset-2">
                                 <img src="https://placeimg.com/192/192/people" />
                             </div>
                         </div>
-                        <div className='w-3/12 flex items-center bg-red-100 absolute bottom-0 text-sm px-2 py-2 text-center rounded-md'><FaStar /><span className='ml-2'>4.8</span></div>
+                        <div className='w-3/12 flex justify-between items-center bg-white absolute bottom-0 py-1 px-2 text-center rounded-md -mb-3 shadow-md'><FaStar className='text-yellow-300'/><span className='text-sm font-semibold'>4.8</span></div>
                     </div>
-                    <h5 className="text-lg font-semibold">Daniel Walter Scott</h5>
-                    <p className="text-gray-200">User Experience Design, <br /> User Interface Design </p>
-                    <div className='flex justify-center'>
+                    <h5 className="text-lg font-semibold mt-7">Daniel Walter Scott</h5>
+                    <p className="text-[#8A8A8A] my-3">User Experience Design, <br /> User Interface Design </p>
+                    <div className='flex justify-center text-xs font-bold'>
                         <div className='text-left'>
-                            <p>Total Students</p>
-                            <p>546,238</p>
+                            <p className='text-[#ABABAB]'>Total Students</p>
+                            <p className='text-[#D76E2D] text-lg'>546,238</p>
                         </div>
-                        <div class="divider lg:divider-horizontal"></div>
+                        <div class="divider lg:divider-horizontal ml-3 mr-3 lg:ml-3 lg:mr-3"></div>
                         <div className='text-left'>
-                            <p>Total Course</p>
-                            <p>30</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='text-center bg-gray-400 rounded-lg py-10'>
-                    <div class="avatar relative">
-                        <div class="w-24 rounded-full ring ring-warning ring-offset-base-100 ring-offset-2">
-                            <img src="https://placeimg.com/192/192/people" />
-                        </div>
-                        {/* <div className='w-8/12 flex items-center mx-auto bg-red-100 py-2 px-3 -mt-4 absolute bottom-0 -mb-6 text-sm'><FaStar /><span className='ml-2'>4.8</span></div> */}
-                    </div>
-                    <h5 className="text-lg font-semibold">Daniel Walter Scott</h5>
-                    <p className="text-gray-200">User Experience Design, <br /> User Interface Design </p>
-                    <div className='flex justify-center'>
-                        <div className='text-left'>
-                            <p>Total Students</p>
-                            <p>546,238</p>
-                        </div>
-                        <div class="divider lg:divider-horizontal"></div>
-                        <div className='text-left'>
-                            <p>Total Course</p>
-                            <p>30</p>
+                            <p className='text-[#ABABAB]'>Total Course</p>
+                            <p className='text-[#D76E2D] text-lg'>30</p>
                         </div>
                     </div>
                 </div>
-                <div className='text-center bg-gray-400 rounded-lg py-10'>
-                    <div class="avatar relative">
-                        <div class="w-24 rounded-full ring ring-warning ring-offset-base-100 ring-offset-2">
-                            <img src="https://placeimg.com/192/192/people" />
+                <div className='text-center bg-white rounded-lg py-6 px-3 border-2'>
+                    <div className='relative flex flex-col items-center'>
+                        <div class="avatar relative">
+                            <div class="w-24 -z-1 rounded-full ring ring-[#D76E2D] ring-offset-base-100 ring-offset-2">
+                                <img src="https://placeimg.com/192/192/people" />
+                            </div>
                         </div>
-                        {/* <div className='w-8/12 flex items-center mx-auto bg-red-100 py-2 px-3 -mt-4 absolute bottom-0 -mb-6 text-sm'><FaStar /><span className='ml-2'>4.8</span></div> */}
-                    </div>
-                    <h5 className="text-lg font-semibold">Daniel Walter Scott</h5>
-                    <p className="text-gray-200">User Experience Design, <br /> User Interface Design </p>
-                    <div className='flex justify-center'>
+                        <div className='w-3/12 flex justify-between items-center bg-white absolute bottom-0 py-1 px-2 text-center rounded-md -mb-3 shadow-md'><FaStar className='text-yellow-300'/><span className='text-sm font-semibold'>4.8</span></div>                    </div>
+                    <h5 className="text-lg font-semibold mt-7">Daniel Walter Scott</h5>
+                    <p className="text-[#8A8A8A] my-3">User Experience Design, <br /> User Interface Design </p>
+                    <div className='flex justify-center text-xs font-bold'>
                         <div className='text-left'>
-                            <p>Total Students</p>
-                            <p>546,238</p>
+                            <p className='text-[#ABABAB]'>Total Students</p>
+                            <p className='text-[#D76E2D] text-lg'>546,238</p>
                         </div>
-                        <div class="divider lg:divider-horizontal"></div>
+                        <div class="divider lg:divider-horizontal ml-3 mr-3 lg:ml-3 lg:mr-3"></div>
                         <div className='text-left'>
-                            <p>Total Course</p>
-                            <p>30</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='text-center bg-gray-400 rounded-lg py-10'>
-                    <div class="avatar relative">
-                        <div class="w-24 rounded-full ring ring-warning ring-offset-base-100 ring-offset-2">
-                            <img src="https://placeimg.com/192/192/people" />
-                        </div>
-                        {/* <div className='w-8/12 flex items-center mx-auto bg-red-100 py-2 px-3 -mt-4 absolute bottom-0 -mb-6 text-sm'><FaStar /><span className='ml-2'>4.8</span></div> */}
-                    </div>
-                    <h5 className="text-lg font-semibold">Daniel Walter Scott</h5>
-                    <p className="text-gray-200">User Experience Design, <br /> User Interface Design </p>
-                    <div className='flex justify-center'>
-                        <div className='text-left'>
-                            <p>Total Students</p>
-                            <p>546,238</p>
-                        </div>
-                        <div class="divider lg:divider-horizontal"></div>
-                        <div className='text-left'>
-                            <p>Total Course</p>
-                            <p>30</p>
+                            <p className='text-[#ABABAB]'>Total Course</p>
+                            <p className='text-[#D76E2D] text-lg'>30</p>
                         </div>
                     </div>
                 </div>
-                <div className='text-center bg-gray-400 rounded-lg py-10'>
-                    <div class="avatar relative">
-                        <div class="w-24 rounded-full ring ring-warning ring-offset-base-100 ring-offset-2">
-                            <img src="https://placeimg.com/192/192/people" />
+                <div className='text-center bg-white rounded-lg py-6 px-3 border-2'>
+                    <div className='relative flex flex-col items-center'>
+                        <div class="avatar relative">
+                            <div class="w-24 -z-1 rounded-full ring ring-[#D76E2D] ring-offset-base-100 ring-offset-2">
+                                <img src="https://placeimg.com/192/192/people" />
+                            </div>
                         </div>
-                        {/* <div className='w-8/12 flex items-center mx-auto bg-red-100 py-2 px-3 -mt-4 absolute bottom-0 -mb-6 text-sm'><FaStar /><span className='ml-2'>4.8</span></div> */}
+                        <div className='w-3/12 flex justify-between items-center bg-white absolute bottom-0 py-1 px-2 text-center rounded-md -mb-3 shadow-md'><FaStar className='text-yellow-300'/><span className='text-sm font-semibold'>4.8</span></div>                    </div>
+                    <h5 className="text-lg font-semibold mt-7">Daniel Walter Scott</h5>
+                    <p className="text-[#8A8A8A] my-3">User Experience Design, <br /> User Interface Design </p>
+                    <div className='flex justify-center text-xs font-bold'>
+                        <div className='text-left'>
+                            <p className='text-[#ABABAB]'>Total Students</p>
+                            <p className='text-[#D76E2D] text-lg'>546,238</p>
+                        </div>
+                        <div class="divider lg:divider-horizontal ml-3 mr-3 lg:ml-3 lg:mr-3"></div>
+                        <div className='text-left'>
+                            <p className='text-[#ABABAB]'>Total Course</p>
+                            <p className='text-[#D76E2D] text-lg'>30</p>
+                        </div>
                     </div>
-                    <h5 className="text-lg font-semibold">Daniel Walter Scott</h5>
-                    <p className="text-gray-200">User Experience Design, <br /> User Interface Design </p>
-                    <div className='flex justify-center'>
-                        <div className='text-left'>
-                            <p>Total Students</p>
-                            <p>546,238</p>
+                </div>
+                <div className='text-center bg-white rounded-lg py-6 px-3 border-2'>
+                    <div className='relative flex flex-col items-center'>
+                        <div class="avatar relative">
+                            <div class="w-24 -z-1 rounded-full ring ring-[#D76E2D] ring-offset-base-100 ring-offset-2">
+                                <img src="https://placeimg.com/192/192/people" />
+                            </div>
                         </div>
-                        <div class="divider lg:divider-horizontal"></div>
+                        <div className='w-3/12 flex justify-between items-center bg-white absolute bottom-0 py-1 px-2 text-center rounded-md -mb-3 shadow-md'><FaStar className='text-yellow-300'/><span className='text-sm font-semibold'>4.8</span></div>                    </div>
+                    <h5 className="text-lg font-semibold mt-7">Daniel Walter Scott</h5>
+                    <p className="text-[#8A8A8A] my-3">User Experience Design, <br /> User Interface Design </p>
+                    <div className='flex justify-center text-xs font-bold'>
                         <div className='text-left'>
-                            <p>Total Course</p>
-                            <p>30</p>
+                            <p className='text-[#ABABAB]'>Total Students</p>
+                            <p className='text-[#D76E2D] text-lg'>546,238</p>
+                        </div>
+                        <div class="divider lg:divider-horizontal ml-3 mr-3 lg:ml-3 lg:mr-3"></div>
+                        <div className='text-left'>
+                            <p className='text-[#ABABAB]'>Total Course</p>
+                            <p className='text-[#D76E2D] text-lg'>30</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='text-center bg-white rounded-lg py-6 px-3 border-2'>
+                    <div className='relative flex flex-col items-center'>
+                        <div class="avatar relative">
+                            <div class="w-24 -z-1 rounded-full ring ring-[#D76E2D] ring-offset-base-100 ring-offset-2">
+                                <img src="https://placeimg.com/192/192/people" />
+                            </div>
+                        </div>
+                        <div className='w-3/12 flex justify-between items-center bg-white absolute bottom-0 py-1 px-2 text-center rounded-md -mb-3 shadow-md'><FaStar className='text-yellow-300'/><span className='text-sm font-semibold'>4.8</span></div>                    </div>
+                    <h5 className="text-lg font-semibold mt-7">Daniel Walter Scott</h5>
+                    <p className="text-[#8A8A8A] my-3">User Experience Design, <br /> User Interface Design </p>
+                    <div className='flex justify-center text-xs font-bold'>
+                        <div className='text-left'>
+                            <p className='text-[#ABABAB]'>Total Students</p>
+                            <p className='text-[#D76E2D] text-lg'>546,238</p>
+                        </div>
+                        <div class="divider lg:divider-horizontal ml-3 mr-3 lg:ml-3 lg:mr-3"></div>
+                        <div className='text-left'>
+                            <p className='text-[#ABABAB]'>Total Course</p>
+                            <p className='text-[#D76E2D] text-lg'>30</p>
                         </div>
                     </div>
                 </div>
